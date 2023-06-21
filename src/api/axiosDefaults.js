5ef8706c8c6fee89e88ddcca1e3d9e6ac51ac96c
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-21 15:46:00 +0000

    #11 Extend token refresh duration to 24 hours

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://travel-tickr-api-e57198555b47.herokuapp.com/' //url to api
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data' // multipart necessary since we have both text and images
 axios.defaults.withCredentials = true
+
+export const axiosReq  = axios.create();
+export const axiosRes = axios.create();
