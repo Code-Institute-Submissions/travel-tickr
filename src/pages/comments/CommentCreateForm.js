7607d02983a6f82af14c73081296a9bb285166fd
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-27 14:19:34 +0000

    #21 Correct typo to show traveler image

--- src/pages/comments/CommentCreateForm.js
+++ src/pages/comments/CommentCreateForm.js
@@ -9,7 +9,7 @@ import Avatar from "../../components/Avatar";
 import { axiosRes } from "../../api/axiosDefaults";
 
 function CommentCreateForm(props) {
-  const { post, setPost, setComments, profileImage, profile_id } = props;
+  const { post, setPost, setComments, travelerImage, traveler_id } = props;
   const [content, setContent] = useState("");
 
   const handleChange = (event) => {
@@ -45,8 +45,8 @@ function CommentCreateForm(props) {
     <Form className="mt-2" onSubmit={handleSubmit}>
       <Form.Group>
         <InputGroup>
-          <Link to={`/profiles/${profile_id}`}>
-            <Avatar src={profileImage} />
+          <Link to={`/travelers/${traveler_id}`}>
+            <Avatar src={travelerImage} />
           </Link>
           <Form.Control
             className={styles.Form}
