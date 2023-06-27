9ff12eaf5fa2c7fd965f1e3b6cced6d8d21d6664
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-27 08:33:11 +0000

    Connect directly to api in gitpod

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://travel-tickr-api-e57198555b47.herokuapp.com/"; //url to api
+// axios.defaults.baseURL = 'https://8000-sandrabergs-traveltickr-f0an5wfcl6d.ws-eu100.gitpod.io/'
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data"; // multipart necessary since we have both text and images
 axios.defaults.withCredentials = true;
 
