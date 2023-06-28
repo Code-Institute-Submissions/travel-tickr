26be736beff8e20dfc3903ae72b6d2a2acb99706
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-28 12:42:07 +0000

    #50 Add popularTravelers.js with function to get from api

---
+++ src/pages/travelers/PopularTravelers.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import appStyles from "../../App.module.css";
+import Container from "react-bootstrap/Container";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
+const PopularTravelers = () => {
+  const [travelerData, setTravelerData] = useState({
+    pageTraveler: { results: [] },
+    popularTravelers: { results: [] },
+  });
+  const { popularTravelers } = travelerData;
+  const currentUser = useCurrentUser()
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/travelers/?ordering=-followers_count"
+        );
+        setTravelerData((prevState) => ({
+            ...prevState,
+            popularTravelers: data,
+        }))
+      } catch (err) {
+        console.log(err)
+      }
+    };
+
+    handleMount()
+  }, [currentUser]);
+
+  return (
+    <Container className={appStyles.Container}>
+      <p>Most followed travelers</p>
+    </Container>
+  );
+};
+
+export default PopularTravelers;
