e2fb4a13aa4740f91eb60d5bd3e07550836384bd
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-28 14:51:43 +0000

    #25 Add follow/unfollow buttons

--- src/pages/travelers/Traveler.js
+++ src/pages/travelers/Traveler.js
@@ -1,30 +1,52 @@
-import React from 'react'
-import styles from '../../styles/TravelerProfile.module.css'
-import btnStyles from '../../styles/Button.module.css'
-import { useCurrentUser } from '../../contexts/CurrentUserContext'
-import { Link } from 'react-router-dom'
-import Avatar from '../../components/Avatar'
+import React from "react";
+import styles from "../../styles/TravelerProfile.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+import { Button } from "react-bootstrap";
 
 const Traveler = (props) => {
-    const {traveler, mobile, imageSize=55} = props
-    const {id, following_id, image, owner} = traveler;
+  const { traveler, mobile, imageSize = 55 } = props;
+  const { id, following_id, image, owner } = traveler;
 
-    const currentUser = useCurrentUser();
-    const is_owner = currentUser?.username === owner;
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
 
   return (
-    <div className={`my-3 d-flex align-items-center ${mobile && 'flex-column'}`}>
-        <div>
-            <Link className="align-self-center" to={`/travelers/${id}`}>
-                <Avatar src={image} height={imageSize} />
-            </Link>
-        </div>
-        <div className={`mx-2 ${styles.WordBreak}`}>
-            <strong>{owner}</strong>
-
-        </div>
+    <div
+      className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
+    >
+      <div>
+        <Link className="align-self-center" to={`/travelers/${id}`}>
+          <Avatar src={image} height={imageSize} />
+        </Link>
+      </div>
+      <div className={`mx-2 ${styles.WordBreak}`}>
+        <strong>{owner}</strong>
+      </div>
+      <div className={`text-right ${!mobile && "ml-auto"}`}>
+        {!mobile &&
+          currentUser &&
+          !is_owner &&
+          (following_id ? (
+            <Button
+              className={`me-1 ${btnStyles.Button} ${btnStyles.Pink}`}
+              onClick={() => {}}
+            >
+              unfollow
+            </Button>
+          ) : (
+            <Button
+              className={`me-1 btn-sm ${btnStyles.Button} ${btnStyles.PinkOutline}`}
+              onClick={() => {}}
+            >
+              follow
+            </Button>
+          ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Traveler
+export default Traveler;
