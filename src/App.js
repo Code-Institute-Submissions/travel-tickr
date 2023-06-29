b6c8a4ed118a9be9f1ac5f62ac65b6f82e5090f8
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-29 07:35:12 +0000

    #51 Add route to travelerpage

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 import PostEditForm from "./pages/posts/PostEditForm";
+import TravelerPage from "./pages/travelers/TravelerPage";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -54,6 +55,7 @@ function App() {
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
           <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
           <Route exact path="/posts/:id" render={() => <PostPage />} />
+          <Route exact path="/travelers/:id" render={() => <TravelerPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
