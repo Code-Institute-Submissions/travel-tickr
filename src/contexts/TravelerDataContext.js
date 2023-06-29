49c0e02cd349239d52b578b8c9cf3a98b72d61ac
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-29 07:00:08 +0000

    #13 Create context provider for traveler data

---
+++ src/contexts/TravelerDataContext.js
@@ -0,0 +1,45 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { axiosReq } from "../api/axiosDefaults";
+import { useCurrentUser } from "./CurrentUserContext";
+
+export const TravelerDataContext = createContext();
+export const SetTravelerDataContext = createContext();
+
+export const useTravelerData = () => useContext(TravelerDataContext);
+export const useSetTravelerDataContext = () =>
+  useContext(SetTravelerDataContext);
+
+export const TravelerDataProvider = ({ children }) => {
+  const [travelerData, setTravelerData] = useState({
+    pageTraveler: { results: [] },
+    popularTravelers: { results: [] },
+  });
+  const { popularTravelers } = travelerData;
+  const currentUser = useCurrentUser();
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/travelers/?ordering=-followers_count"
+        );
+        setTravelerData((prevState) => ({
+          ...prevState,
+          popularTravelers: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [currentUser]);
+};
+
+return (
+    <TravelerDataContext.Provider value={travelerData}>
+        <SetTravelerDataContext.Provider value={setTravelerData}>
+            {children}
+        </SetTravelerDataContext.Provider>
+    </TravelerDataContext.Provider>
+)
