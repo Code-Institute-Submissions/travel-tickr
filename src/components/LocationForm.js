e524b7c755c6f98df37b56b61d122710d14adea0
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-06-30 07:47:09 +0000

    #36 Add basic structure for location component

---
+++ src/components/LocationForm.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import axios from 'axios';
+
+const LocationForm = () => {
+
+}
+
+return (
+    <form>
+        <div>
+            <label>Name of location:</label>
+            <input />
+        </div>
+        <div>
+            <label>Country:</label>
+            <select>
+                
+            </select>
+        </div>
+    </form>
+)
+
+export default LocationForm;
