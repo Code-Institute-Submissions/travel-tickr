66160ecbc5bf6accb5cede81a405323e402cb4d6
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-07-04 18:57:37 +0000

    #56 Automated testing loginform

---
+++ src/pages/auth/__tests__/LoginForm.test.js
@@ -0,0 +1,24 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import LogInForm from "../LoginForm";
+
+test("renders Login page ", () => {
+  render(
+    <Router>
+      <LogInForm />
+    </Router>
+  );
+
+  //   screen.debug();
+
+  // Check to see if username field is rendered to the user
+  const usernameField = screen.getByPlaceholderText('Enter username');
+  expect(usernameField).toBeInTheDocument();
+
+  const passwordField = screen.getByPlaceholderText('Password');
+  expect(passwordField).toBeInTheDocument();
+
+  const submitButton = screen.getByRole("button", { name: "Login" });
+  expect(submitButton).toBeInTheDocument();
+
+});
