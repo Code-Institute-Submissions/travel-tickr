01a0ef65a0c4077d6a50774c783ad8ff397475b4
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-07-05 07:26:30 +0000

    #57 Add const set, refresh and remove Tokentimestamp

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,4 @@
+import jwtDecode from "jwt-decode";
 import { axiosReq } from "../api/axiosDefaults";
 
 export const fetchMoreData = async (resource, setResource) => {
@@ -22,7 +23,7 @@ export const followHelper = (traveler, clickedTraveler, following_id) => {
       {
         ...traveler,
         followers_count: traveler.followers_count + 1,
-        following_id
+        following_id,
       }
     : traveler.is_owner
     ? // If this is logged in user, update it's following count
@@ -34,12 +35,25 @@ export const followHelper = (traveler, clickedTraveler, following_id) => {
 
 export const unfollowHelper = (traveler, clickedTraveler) => {
   return traveler.id === clickedTraveler.id
-  ? {
-    ...traveler,
-    followers_count: traveler.followers_count -1,
-    following_id: null,
-  }
-  : traveler.is_owner
-  ? {...traveler, following_count: traveler.following_count -1 }
-  : traveler;
-}
+    ? {
+        ...traveler,
+        followers_count: traveler.followers_count - 1,
+        following_id: null,
+      }
+    : traveler.is_owner
+    ? { ...traveler, following_count: traveler.following_count - 1 }
+    : traveler;
+};
+
+export const setTokenTimestamp = (data) => {
+  const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
+  localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp);
+};
+
+export const shouldRefreshToken = () => {
+  return !localStorage.getItem("refreshTokenTimestamp");
+};
+
+export const removeTokenTimestamp = () => {
+  localStorage.removeItem("refreshTokenTimestamp");
+};
